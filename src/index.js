a6825b66b8fd5222bc6a4e7faaf3cd3ec65ec144
Kurt Duyck <kurt.duyck@vives.be>
2021-02-02 10:33:04 +0100

    Inject some html
    	modified:   CHANGELOG.md
    	modified:   package.json
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('Interesting!')
+// Create heading node
+const heading = document.createElement('h1')
+heading.textContent = 'Interesting!'
+
+// Append heading node to the DOM
+const app = document.querySelector('#root')
+app.append(heading)
