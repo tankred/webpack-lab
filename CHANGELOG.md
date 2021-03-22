b30ee33cbb586fdf82f468a7ff6140a2f749fd0e
Kurt Duyck <kurt.duyck@vives.be>
2021-03-22 09:38:06 +0100

    Minify css for producion
    	modified:   CHANGELOG.md
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/styles/main.scss
    	modified:   webpack.common.js
    	modified:   webpack.prod.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.3.3] - 2021-03-22
+### Changed
+- Minify CSS for production
+
 ## [0.3.2] - 2021-03-19
 ### Changed
 - Build for production
--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const webpack = require('webpack')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -35,6 +36,12 @@ module.exports = {
         test: /\.(scss|css)$/,
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
     ],
   },
   output: {
